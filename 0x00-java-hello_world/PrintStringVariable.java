3be6efbfc68b0215abd88c2cd21b42aeb1c7259a
gabrielamanoelc <gabrielamanoel@ciandt.com>
2022-05-04 11:27:24 -0700

    g

---
+++ 0x00-java-hello_world/PrintStringVariable.java
@@ -0,0 +1,6 @@
+public class Println {
+    public static void main(String[] args) {
+        String texto = "Holberton School";
+        System.out.println(texto + "\n" + texto + "\n" + texto.replace(" School", ""));
+    }
+}
