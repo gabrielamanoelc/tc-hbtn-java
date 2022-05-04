54fd86f882e2dfdd4203be2d17c331f45e32e613
gabrielamanoelc <gabrielamanoel@ciandt.com>
2022-05-04 11:23:19 -0700

    g

---
+++ 0x00-java-hello_world/PrintIntegerVariable.java
@@ -0,0 +1,10 @@
+package trace;
+
+public class Println {
+
+    public static void main(String[] args) {
+
+        int numero = 311;
+        System.out.println("Avenida Andaló, " + numero);
+    }
+}
