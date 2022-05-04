3974ce39319c22164f4d1e1558a7915280edd164
gabrielamanoelc <gabrielamanoel@ciandt.com>
2022-05-04 12:00:21 -0700

    g

---
+++ 0x01/maiusculo/Caractere.java
@@ -0,0 +1,9 @@
+public class Caractere {
+	public static void main(String[] args) {
+        
+	}
+
+    public static boolean EhMaiusculo(char c){
+        return Character.isUpperCase(c);
+    }
+}
