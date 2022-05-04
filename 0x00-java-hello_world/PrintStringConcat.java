8b3adbadb36a2a1937553b7672bd83cbac1e47c6
gabrielamanoelc <gabrielamanoel@ciandt.com>
2022-05-04 11:28:58 -0700

    g

---
+++ 0x00-java-hello_world/PrintStringConcat.java
@@ -0,0 +1,8 @@
+public class Println {
+    public static void main(String[] args) {
+        String texto1 = "Holberton";
+        String texto2 = "School";
+        String result = "Bem vindo a " + texto1 + " " + texto2 + "!";
+        System.out.println(result);
+    }
+}
