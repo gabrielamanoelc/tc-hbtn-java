1e2bcaca12d695adc757a85057cf5b1822a8d776
gabrielamanoelc <gabrielamanoel@ciandt.com>
2022-05-05 15:23:39 -0700

    g

---
+++ 0x03/conta_basica/exceptions/OperacaoInvalidaException.java
@@ -0,0 +1,16 @@
+package exceptions;
+
+public class OperacaoInvalidaException extends Exception{
+
+    private String mensagem;
+
+    public OperacaoInvalidaException(String mensagem) {
+        super(mensagem);
+        this.mensagem = mensagem;
+    }
+
+    @Override
+    public String toString() {
+        return mensagem;
+    }
+}
