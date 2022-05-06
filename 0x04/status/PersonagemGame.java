67c89c854904c92628b23b7aef390c8cbf5a3ec6
gabrielamanoelc <gabrielamanoel@ciandt.com>
2022-05-06 05:59:00 -0700

    g

---
+++ 0x04/status/PersonagemGame.java
@@ -0,0 +1,51 @@
+public class PersonagemGame {
+
+    private int saudeAtual;
+    private String nome;
+
+    private String status;
+
+    public int getSaudeAtual() {
+        return saudeAtual;
+    }
+
+    public void setSaudeAtual(int saudeAtual) {
+        this.saudeAtual = saudeAtual;
+        if(this.saudeAtual > 0){
+            this.status = "vivo";
+        } else {
+            this.status = "morto";
+        }
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void tomarDano(int quantidadeDeDano){
+        if(this.saudeAtual - quantidadeDeDano < 0){
+            this.saudeAtual = 0;
+            setSaudeAtual(0);
+        } else {
+            setSaudeAtual(this.saudeAtual -= quantidadeDeDano);
+        }
+    }
+
+    public void receberCura(int quantidadeDeCura){
+        if(this.saudeAtual + quantidadeDeCura > 100){
+            this.saudeAtual = 100;
+            setSaudeAtual(100);
+        } else {
+            setSaudeAtual(this.saudeAtual += quantidadeDeCura);
+        }
+
+    }
+}
