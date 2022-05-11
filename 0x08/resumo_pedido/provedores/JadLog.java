1157243d8459c697d4da78c7aa7ca89f468532e1
gabrielamanoelc <gabrielamanoel@ciandt.com>
2022-05-11 16:58:56 -0700

    g

---
+++ 0x08/resumo_pedido/provedores/JadLog.java
@@ -0,0 +1,24 @@
+package provedores;
+
+public class JadLog implements ProvedorFrete{
+
+    Frete frete = new Frete();
+    @Override
+    public Frete calcularFrete(double peso, double valor) {
+
+        if(peso > 2000){
+            valor = (valor * 0.07);
+        } else {
+            valor = (valor * 0.045);
+        }
+
+        frete.setValor(valor);
+        frete.setTipoProvedorFrete(TipoProvedorFrete.JADLOG);
+        return frete;
+    }
+
+    @Override
+    public TipoProvedorFrete obterTipoProvedorFrete() {
+        return frete.getTipoProvedorFrete();
+    }
+}
